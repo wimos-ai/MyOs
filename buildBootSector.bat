fa935886adc62a9706a18b893a9f84afbc197222
willm <willmoiser@gmail.com>
2022-03-14 18:39:57 -0500

    Updated build file

--- buildBootSector.bat
+++ buildBootSector.bat
@@ -1,8 +1,8 @@
+@echo off
 set boot-sector="src\asm\bootSector.asm"
 set bios-print-str="src\asm\BiosPrintString.asm"
 set out-name="out\bootSector.bin"
 set nasm="nasm-2.15.05\nasm.exe"
 
-
-%nasm% %boot-sector% -f bin -o %out-name%
-pause -> nul
+@echo on
+%nasm% %boot-sector% -f bin -i "src/asm" -o %out-name%
