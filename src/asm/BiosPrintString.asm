47152d45ffc76ddf9af349a8ed22437098923de8
willm <willmoiser@gmail.com>
2022-03-14 18:40:16 -0500

    Added more flexibility to bios programming

--- src/asm/BiosPrintString.asm
+++ src/asm/BiosPrintString.asm
@@ -1,15 +1,14 @@
 BIOSPrintStr:
-	add bx, 0x7c00  ;Adds the offset
-	mov ah, 0x0e    ;Sets the propper interupt flag for scrolling teletype
+	mov ah, 0x0e    ;Sets the proper interrupt flag for scrolling teletype
 	
 	beginLoop:
-		mov al , [bx] ;Loads the charecter to al
+		mov al , [bx] ;Loads the character to al
 		
 		; Checks to see if al is zero
 		cmp al , 0
 		jz return ;if al is zero, we jump to the return segment
 		
-		int 0x10      ; Triggers printing interupt
+		int 0x10      ; Triggers printing interrupt
 		add bx, 1     ;increments bx
 		
 		jmp beginLoop ; cycles back through the loop
--- src/asm/bootSector.asm
+++ src/asm/bootSector.asm
@@ -1,3 +1,9 @@
+[BITS 16]
+[ORG 0x7C00]
+
+jmp bootSectorEntryPt
+
+%include "BiosPrintString.asm"
 
 section	.text
 ;
@@ -16,25 +22,7 @@ bootSectorEntryPt:
 	
 	jmp $
 	
-	
-BIOSPrintStr:
-	add bx, 0x7c00  ;Adds the offset
-	mov ah, 0x0e    ;Sets the propper interupt flag for scrolling teletype
-	
-	beginLoop:
-		mov al , [bx] ;Loads the charecter to al
-		
-		; Checks to see if al is zero
-		cmp al , 0
-		jz return ;if al is zero, we jump to the return segment
-		
-		int 0x10      ; Triggers printing interupt
-		add bx, 1     ;increments bx
-		
-		jmp beginLoop ; cycles back through the loop
 
-	return:
-		retn
 	
 string:
 	db "I am a string",0xa,0xd,0
